c94313028076a677af47f6c9b2b40798cd503af6
Krishna Priya S <krishnapriyask2007@gmail.com>
2026-05-26 19:14:16 +0530

    Create xor_gate_tb.v

---
+++ xor_gate_tb.v
@@ -0,0 +1,15 @@
+//testbench for xor gate
+module testbench;
+  reg a,b;
+  wire y;
+  xor_gate uut (.a(a),.b(b),.y(y));
+  initial begin
+    $monitor("a=%b,b=%b,y=%b",a,b,y);
+    $dumpfile("dump.vcd");
+    $dumpvars(0,testbench);
+    a=0;b=0;#10;
+    a=0;b=1;#10;
+    a=1;b=0;#10;
+    a=1;b=1;#10;
+  end
+endmodule
