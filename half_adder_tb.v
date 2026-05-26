8815f14d5f9d7872ae6934cedfc6e887b7420ac7
Krishna Priya S <krishnapriyask2007@gmail.com>
2026-05-26 20:39:54 +0530

    Create half_adder_tb.v

---
+++ half_adder_tb.v
@@ -0,0 +1,15 @@
+//testbench for half adder
+module testbench;
+  reg a,b;
+  wire sum,carry;
+  half_adder uut(.a(a),.b(b),.sum(sum),.carry(carry));
+  initial begin
+    $monitor("a=%b,b=%b,sum=%b,carry=%b",a,b,sum,carry);
+    $dumpfile("dump.vcd");
+    $dumpvars(0,testbench);
+    a=0;b=0;#10;
+    a=0;b=1;#10;
+    a=1;b=0;#10;
+    a=1;b=1;#10;
+  end
+endmodule
