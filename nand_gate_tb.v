40c566967034b66d255284c4efde83334e6172cc
Krishna Priya S <krishnapriyask2007@gmail.com>
2026-05-26 19:06:31 +0530

    Create nand_gate_tb.v

---
+++ nand_gate_tb.v
@@ -0,0 +1,15 @@
+//testbench for Nand gate
+module testbench;
+  reg a,b;
+  wire y;
+  nand_gate uut (.a(a),.b(b),.y(y));
+  initial begin
+    $monitor("a=%b,b=%b,y=%b",a,b,y);
+    $dumpfile("dump.vcd");
+    $dumpvars(0,testbench);
+    a=0;b=0;#10;
+    a=0;b=1;#10;
+    a=1;b=0;#10;
+    a=1;b=1;#10;
+  end
+endmodule
